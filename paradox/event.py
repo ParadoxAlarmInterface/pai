04526028fa44782b99f6de58377647e46266a64f
João Paulo Barraca <jpbarraca@gmail.com>
2019-01-11 09:29:04 +0000

    Add code to handle partition id and element id in events

--- paradox/event.py
+++ paradox/event.py
@@ -31,6 +31,7 @@ class Event:
         self.message_tpl = ''
         self.change = {}
         self.additional_data = {}
+        self.partition = None
 
         if event is not None:
             self.parse(event, names)
@@ -47,17 +48,18 @@ class Event:
         if event.fields.value.po.command != 0x0e:
             raise(Exception("Invalid Event"))
 
-        self.raw = copy(event.fields.value)
-        self.timestamp = self.raw.time
-        self.partition = self.raw.partition
-        self.module = self.raw.module_serial
-        self.label_type = self.raw.get('label_type', None)
-        self.label = self.raw.label.strip(b'\0 ').decode('utf-8')
+        self.raw = copy(event.fields.value)  # Event raw data
+        self.timestamp = self.raw.time  # Event timestamp
+        self.partition = self.raw.partition  # Event Partition. if Type is system, partition may not be relevant
+        self.module = self.raw.module_serial  # Event Module Serial
+        self.label_type = self.raw.get('label_type', None)  # Type of element triggering the event
+        self.label = self.raw.label.strip(b'\0 ').decode('utf-8')  # Event Element Label. May be overwride localy
 
-        self.major = self.raw.event.major
-        self.minor = self.raw.event.minor
+        self.major = self.raw.event.major  # Event major code
+        self.minor = self.raw.event.minor  # Event minor code
+
         self._names = names or {}
-
+        self.id = None  # Element ID (if available in the event)
         self._parse_map(names)
 
     def _parse_map(self, names):
@@ -66,19 +68,24 @@ class Event:
 
         event_map = copy(self._event_map[self.major])  # for inplace modifications
 
-        if 'sub' in event_map and self.minor in event_map['sub']:
-            sub = event_map['sub'][self.minor]
-            if isinstance(sub, str):
-                sub = dict(message=sub)
+        # If 'sub' key is present, minor code will be used to obtain detailed information
+        if 'sub' in event_map:
+            if self.minor in event_map['sub']:
+                sub = event_map['sub'][self.minor]
+                if isinstance(sub, str):
+                    sub = dict(message=sub)
 
-            for k in sub:
-                if k == 'message':
-                    event_map[k] = '{}: {}'.format(event_map[k], sub[k]) if k in event_map else sub[k]
-                elif isinstance(sub[k], typing.List):  # for tags or other lists
-                    event_map[k] = event_map.get(k, []).extend(sub[k])
-                else:
-                    event_map[k] = sub[k]
-            del event_map['sub']
+                for k in sub:
+                    if k == 'message':
+                        event_map[k] = '{}: {}'.format(event_map[k], sub[k]) if k in event_map else sub[k]
+                    elif isinstance(sub[k], typing.List):  # for tags or other lists
+                        event_map[k] = event_map.get(k, []).extend(sub[k])
+                    else:
+                        event_map[k] = sub[k]
+                del event_map['sub']
+        # If key is not present, minor code contains the element ID
+        else:
+            self.id = self.minor
 
         callables = (k for k in event_map if isinstance(event_map[k], typing.Callable))
         for k in callables:
@@ -91,6 +98,11 @@ class Event:
         self.tags = event_map.get('tags', [])
 
         self.additional_data = {k: v for k, v in event_map.items() if k not in ['message'] and not hasattr(self, k)}
+
+        # Set partition label
+        if self.type == 'partition' and 'partition' in names:
+            self.label = names['partition'].get(self.partition, self.label)
+            self.id = self.partition
 
     @property
     def message(self):
