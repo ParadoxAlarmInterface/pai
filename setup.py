1edbbc5a8bc4c7e5be207201f385ad60f933bb23
Jevgeni Kiski <yozik04@gmail.com>
2019-03-03 19:35:34 +0200

    Test dependencies and pytest runner.

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
+import sys
 import setuptools
+
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
+
+    def run_tests(self):
+        import shlex
+
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -14,9 +33,15 @@ setuptools.setup(
     url="https://github.com/jpbarraca/pai",
     packages=['config', 'paradox'],
     install_requires=[],
+    tests_require=[
+        'pytest',
+        'pytest-mock',
+        'mock'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: EPL License",
         "Operating System :: OS Independent",
     ],
+    cmdclass={"pytest": PyTest},
 )
