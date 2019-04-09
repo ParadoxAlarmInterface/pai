95a6e63005389eaae82c1735f5a40f6ff9685115
Jevgeni Kiski <yozik04@gmail.com>
2019-04-09 15:03:28 +0300

    More tests for reading chunks from the serial.

--- tests/evo/test_serial_protocol.py
+++ tests/evo/test_serial_protocol.py
@@ -35,3 +35,38 @@ def test_37byte_message_in_chunks(mocker):
     cp.data_received(payload1)
 
     cp.read_queue.put_nowait.assert_called_with(payload + payload1)
+
+def test_37byte_message_in_many_chunks(mocker):
+    cp = SerialConnectionProtocol(None, None)
+
+    payloads = [
+        b'\xe2\xff\xad\x06\x14',
+        b'\x13\x01\x04\x0e\x10',
+        b'\x00\x01\x05\x00\x00',
+        b'\x00\x00\x00\x02Liv',
+        b'ing room     \x00\xcc'
+    ]
+    mocker.patch.object(cp, 'read_queue')
+
+    for p in payloads:
+        cp.data_received(p)
+
+    cp.read_queue.put_nowait.assert_called_with(b"".join(payloads))
+
+def test_37byte_message_in_many_chunks_with_junk_in_front(mocker):
+    cp = SerialConnectionProtocol(None, None)
+
+    payloads = [
+        b'\x01\x02\x03\x04\x05\x06\x07',
+        b'\x01\x02\xe2\xff\xad\x06\x14',
+        b'\x13\x01\x04\x0e\x10',
+        b'\x00\x01\x05\x00\x00',
+        b'\x00\x00\x00\x02Liv',
+        b'ing room     \x00\xcc'
+    ]
+    mocker.patch.object(cp, 'read_queue')
+
+    for p in payloads:
+        cp.data_received(p)
+
+    cp.read_queue.put_nowait.assert_called_with(b"".join(payloads)[9:])
