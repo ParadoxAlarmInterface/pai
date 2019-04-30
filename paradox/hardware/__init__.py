4f940a934a9674e78b42d986b13522c190b1eb6f
Jevgeni Kiski <yozik04@gmail.com>
2019-04-30 09:42:00 +0300

    #70 Adding SPECTRA_SP4000 to supported

--- paradox/hardware/__init__.py
+++ paradox/hardware/__init__.py
@@ -16,7 +16,7 @@ def create_panel(core, product_id=None) -> Panel:
     elif product_id == 'DIGIPLEX_EVO_HD':
         from . import evo
         return evo.Panel_EVOHD(core, product_id)
-    elif product_id in ['SPECTRA_SP5500', 'SPECTRA_SP6000', 'SPECTRA_SP7000', 'MAGELLAN_MG5000', 'MAGELLAN_MG5050']:
+    elif product_id in ['SPECTRA_SP4000', 'SPECTRA_SP5500', 'SPECTRA_SP6000', 'SPECTRA_SP7000', 'MAGELLAN_MG5000', 'MAGELLAN_MG5050']:
         from . import spectra_magellan
         return spectra_magellan.Panel(core, product_id)
     else:
