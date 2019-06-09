260f5bbf85211a036f9b5019d4a445dbdff6205a
Jevgeni Kiski <yozik04@gmail.com>
2019-06-09 15:50:52 +0300

    SPECTRA_SP65 support. Fixes #85

--- paradox/hardware/__init__.py
+++ paradox/hardware/__init__.py
@@ -16,7 +16,11 @@ def create_panel(core, product_id=None) -> Panel:
     elif product_id == 'DIGIPLEX_EVO_HD':
         from . import evo
         return evo.Panel_EVOHD(core, product_id, True)
-    elif product_id in ['SPECTRA_SP4000', 'SPECTRA_SP5500', 'SPECTRA_SP6000', 'SPECTRA_SP7000', 'MAGELLAN_MG5000', 'MAGELLAN_MG5050']:
+    elif product_id in [
+        'SPECTRA_SP4000', 'SPECTRA_SP5500', 'SPECTRA_SP6000', 'SPECTRA_SP7000',
+        'SPECTRA_SP65',
+        'MAGELLAN_MG5000', 'MAGELLAN_MG5050'
+    ]:
         from . import spectra_magellan
         return spectra_magellan.Panel(core, product_id, False)
     else:
--- paradox/hardware/common.py
+++ paradox/hardware/common.py
@@ -22,6 +22,7 @@ ProductIdEnum = Enum(Int8ub,
                      SPECTRA_SP6000=22,
                      SPECTRA_SP7000=23,
                      SPECTRA_SP4000=26,
+                     SPECTRA_SP65=27,
                      MAGELLAN_MG5000=64,
                      MAGELLAN_MG5050=65
                      )
