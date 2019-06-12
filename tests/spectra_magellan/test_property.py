adbcc2163caeaa9680f673d568bcfce542ddf7bb
Jevgeni Kiski <yozik04@gmail.com>
2019-06-12 14:49:19 +0300

    Converted to Parametrized test.

--- tests/spectra_magellan/test_property.py
+++ tests/spectra_magellan/test_property.py
@@ -1,8 +1,28 @@
 import time
 import os
+import pytest
 
 from paradox.event import Event
 from paradox.hardware.spectra_magellan import Panel
+
+def generate_property_test_parameters():
+    logfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'properties.log')
+    with open(logfile, 'r') as f:
+        for line in f:
+            aux = line.strip().split(' ')
+            if aux[1] in ["True", "False"]:
+                values = [True, False]
+            else:
+                values = [float(aux[1])]
+
+            aux = aux[0].split("/")
+            partition = aux[1] if aux[0] == "partitions" else None
+
+            for value in values:
+                property = aux[2]
+                label = aux[1]
+                type = aux[0]
+                yield pytest.param(type, property, value, partition, label)
 
 def test_property_map_value():
 
@@ -18,26 +38,12 @@ def test_property_map_bad():
     r = evt.from_change(change=change, property_map=Panel.property_map)
     assert not r
 
+@pytest.mark.parametrize("type,property,value,partition,label", generate_property_test_parameters())
+def test_property(type, property, value, partition, label):
+    change = dict(property=property, value=value, partition=partition, time=time.time(), type=type, label=label)
+    evt = Event()
+    r = evt.from_change(change=change, property_map=Panel.property_map)
 
-def test_all_properties():
-    logfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'properties.log')
-    with open(logfile, 'r') as f:
-        for line in f:
-            aux = line.strip().split(' ')
-            if aux[1] in ["True", "False"]:
-                values = [True, False]
-            else:
-                values = [float(aux[1])]
-
-            aux = aux[0].split("/")
-            partition = aux[1] if aux[0] == "partitions" else None
-
-            for value in values:
-
-                change = dict(property=aux[2], value=value, partition=partition, time=time.time(), type=aux[0], label=aux[1])
-                evt = Event()
-                r = evt.from_change(change=change, property_map=Panel.property_map)
-
-                assert r
-                assert len(evt.message) > 0
+    assert r
+    assert len(evt.message) > 0
 
