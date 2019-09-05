b5ed0331bc6099d56316a8180a416e395dfdbb5f
psyciknz <github@andc.co.nz>
2019-09-05 15:59:50 +1200

    Trying to add testing, and get labels working

--- tests/interfaces/test_homie_interface.py
+++ tests/interfaces/test_homie_interface.py
@@ -1,16 +1,47 @@
-from mock import call
+import pytest
 from pytest_mock import mocker
 
 from paradox.interfaces.homie_mqtt_interface import HomieMQTTInterface
 
-def test_handle_panel_change(mocker):
-    cp = HomieMQTTInterface(mocker)
+from paradox.lib.ps import sendMessage
 
-    change = {}
-    change['property'] = 'open'
-    change['label'] = "Zone 01"
-    change['value'] = True
-    change['initial'] = True
-    change['type'] = 'zone'
 
-    cp.handle_panel_change(change)
+# def test_handle_panel_change(mocker):
+#     cp = HomieMQTTInterface(mocker)
+
+#     change = {}
+#     change['property'] = 'open'
+#     change['label'] = "Zone 01"
+#     change['value'] = True
+#     change['initial'] = True
+#     change['type'] = 'zone'
+
+#     cp.handle_panel_change(change)
+
+def send_initial_status():
+    sendMessage("labels_loaded", data=dict(
+        partition={
+            1: dict(
+                id=1,
+                label='Partiton 1',
+                key='Partiton_1'
+            )
+        }
+    ))
+
+    sendMessage("status_update", status=dict(
+        partition_status={
+            1: dict(
+                arm=False
+            )
+        }
+    ))
+
+@pytest.mark.asyncio
+async def test_homie_pending(mocker):
+    interface = HomieMQTTInterface()
+    mocker.patch.object(interface, "mqtt")
+    interface.start()
+
+    await interface._started.wait()
+    send_initial_status()
