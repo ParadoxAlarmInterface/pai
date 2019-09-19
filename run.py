faf877379bb50aa7d59f47961f78427d38eeb022
João Paulo Barraca <jpbarraca@gmail.com>
2019-09-19 23:14:36 +0100

    Changed location of header info

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "João Paulo Barraca, Jevgeni Kiski"
+__copyright__ = "Copyright 2018-2019, João Paulo Barraca"
+__credits__ = ["Tihomir Heidelberg", "Louis Rossouw"]
+__license__ = "EPL"
+__version__ = "0.1"
+__maintainer__ = "João Paulo Barraca"
+__email__ = "jpbarraca@gmail.com"
+__status__ = "Beta"
+
 from paradox.lib import help
 
 try:
