812677726a2764dbbfeefe4f8d6fcc3f211be777
João Paulo Barraca <jpbarraca@gmail.com>
2019-09-19 23:05:07 +0100

    Added some help on missing modules

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from paradox.console_scripts.pai_run import main
+from paradox.lib import help
+
+try:
+	from paradox.console_scripts.pai_run import main
+except ImportError as error:
+    help.import_error_help(error)
 
 if __name__ == '__main__':
     main()
+    
