da1bdbe70681357473b5913d77c8af01125febaa
Jevgeni Kiski <yozik04@gmail.com>
2019-10-24 17:11:11 +0300

    Test fix

--- tests/interfaces/mqtt/test_homeassistant.py
+++ tests/interfaces/mqtt/test_homeassistant.py
@@ -1,4 +1,5 @@
 import pytest
+import asyncio
 from mock import MagicMock
 
 from paradox.interfaces.mqtt.homeassistant import HomeAssistantMQTTInterface
@@ -12,39 +13,40 @@ async def test_hass():
     interface.mqtt = MagicMock()
     interface.start()
 
-    await interface._started.wait()
+    try:
+        await asyncio.sleep(0.1)  # TODO: Bad way to wait for a start
 
-    sendMessage("labels_loaded", data=dict(
-        partition={
-            1: dict(
-                id=1,
-                label='Partition 1',
-                key='Partition_1'
+        sendMessage("labels_loaded", data=dict(
+            partition={
+                1: dict(
+                    id=1,
+                    label='Partition 1',
+                    key='Partition_1'
+                )
+            }
+        ))
+
+        sendMessage("status_update", status=dict(
+            partition={
+                1: dict(
+                    arm=False
+                )
+            }
+        ))
+
+        interface.mqtt.publish.assert_called_with('homeassistant/alarm_control_panel/pai/Partition_1/config', json.dumps(
+            dict(
+                name='Partition 1',
+                unique_id="pai_partition_Partition_1",
+                command_topic='paradox/control/partitions/Partition_1',
+                state_topic='paradox/states/partitions/Partition_1/current_state',
+                availability_topic='paradox/interface/MQTTInterface',
+                device=dict(),
+                payload_disarm="disarm",
+                payload_arm_home="arm_stay",
+                payload_arm_away="arm",
+                payload_arm_night="arm_sleep"
             )
-        }
-    ))
-
-    sendMessage("status_update", status=dict(
-        partition={
-            1: dict(
-                arm=False
-            )
-        }
-    ))
-
-    interface.mqtt.publish.assert_called_with('homeassistant/alarm_control_panel/pai/Partition_1/config', json.dumps(
-        dict(
-            name='Partition 1',
-            unique_id="pai_partition_Partition_1",
-            command_topic='paradox/control/partitions/Partition_1',
-            state_topic='paradox/states/partitions/Partition_1/current_state',
-            availability_topic='paradox/interface/MQTTInterface',
-            device=dict(),
-            payload_disarm="disarm",
-            payload_arm_home="arm_stay",
-            payload_arm_away="arm",
-            payload_arm_night="arm_sleep"
-        )
-    ), 0, True)
-
-    interface.stop()
+        ), 0, True)
+    finally:
+        interface.stop()
