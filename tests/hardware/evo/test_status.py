adca6e486c032d51f4c8e868bb8f9f2bbe0d12b1
Jevgeni Kiski <yozik04@gmail.com>
2019-11-03 00:28:12 +0200

    test EVO status.

---
+++ tests/hardware/evo/test_status.py
@@ -0,0 +1,56 @@
+import mock
+import json
+
+from paradox.paradox import Paradox
+from paradox.parsers.status import convert_raw_status
+
+from .status_data import message_parser_output, converted_status
+
+
+def test_convert_raw_status(mocker):
+    mocker.patch('paradox.lib.ps')
+    p = Paradox()
+    status = convert_raw_status(message_parser_output)
+
+    assert status["zone"] == {
+        1: dict(open=False, tamper=False, low_battery=False, generated_alarm=False, presently_in_alarm=False,
+                activated_entry_delay=False,
+                activated_intellizone_delay=False, bypassed=False, shutted_down=False,
+                tx_delay=False, supervision_trouble=False),
+        2: dict(open=False, tamper=False, low_battery=False, generated_alarm=False,
+                presently_in_alarm=False,
+                activated_entry_delay=False,
+                activated_intellizone_delay=False,
+                bypassed=False,
+                shutted_down=False,
+                tx_delay=False,
+                supervision_trouble=False)
+    }
+
+    assert status["partition"] == message_parser_output["partition_status"]
+    assert status["door"] == {1: dict(open=False), 2: dict(open=False)}
+    assert status["bus-module"] == {1: dict(trouble=False), 2: dict(trouble=False)}
+    assert status["system"]["troubles"] == message_parser_output["system"]["troubles"]
+
+    a = json.dumps(converted_status, sort_keys=True, indent=2, default=str)
+    b = json.dumps(status, sort_keys=True, indent=2, default=str)
+    assert a == b
+
+    # import deepdiff
+    # from pprint import pprint
+    # for key, value in status.items():
+    #     try:
+    #         result = deepdiff.DeepDiff(status[key].store, converted_status[key], ignore_order=True, ignore_type_subclasses=True)
+    #         pprint(result)
+    #     except KeyError as e:
+    #         raise
+
+
+def test_update_properties(mocker):
+    alarm = Paradox()
+    alarm.panel = mocker.MagicMock()
+    ps = mocker.patch("paradox.lib.ps")
+
+    alarm._on_status_update(converted_status)
+
+    print(ps)
