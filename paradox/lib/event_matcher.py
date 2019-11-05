9d39589f764d4776abab7156d5d13ff2771ecaf9
Jevgeni Kiski <yozik04@gmail.com>
2019-11-05 15:56:44 +0200

    Advanced event matcher. Looks into label and label from label_provider.

--- paradox/lib/event_matcher.py
+++ paradox/lib/event_matcher.py
@@ -1,15 +1,20 @@
 import logging
 import re
 
-from paradox.event import Event
+from paradox.event import Event, LiveEvent
 
-re_match = re.compile(r'[+-]?[a-z]+')
+re_match = re.compile(r"[+-]?(?P<quote>['\"])?(?(quote).+?|[a-z_A-Z]+)(?(quote)(?P=quote))")
+re_unquote = re.compile(r"^(?P<quote>['\"])(.*)(?P=quote)$")
 
 logger = logging.getLogger('PAI').getChild(__name__)
 
+def _unquote(s:str):
+    return re_unquote.sub(r'\2', s)
 
 def tag_match(event: Event, queries:list):
     tags = [event.type] + event.tags
+    if isinstance(event, LiveEvent):
+        tags += [event.name, event.label]
 
     for query in queries:
         include = set()
@@ -17,11 +22,11 @@ def tag_match(event: Event, queries:list):
         for m in re_match.finditer(query):
             token = m.group()
             if token[0] == '+':
-                include.add(token[1:])
+                include.add(_unquote(token[1:]))
             elif token[0] == '-':
-                exclude.add(token[1:])
+                exclude.add(_unquote(token[1:]))
             else:
-                include.add(token)
+                include.add(_unquote(token))
 
         if all(t in tags for t in include) and all(t not in tags for t in exclude):
             return True
--- tests/lib/test_event_matcher.py
+++ tests/lib/test_event_matcher.py
@@ -1,5 +1,9 @@
+import binascii
+
+from paradox.event import Event, LiveEvent
+from paradox.hardware.evo.event import event_map
+from paradox.hardware.evo.parsers import LiveEvent as LiveEventMessage
 from paradox.lib.event_matcher import tag_match
-from paradox.event import Event
 
 
 def test_tag_match():
@@ -16,3 +20,31 @@ def test_tag_match():
     assert tag_match(event, ['arm-zone+restore+partition']) is True
     assert tag_match(event, ['zone']) is False
     assert tag_match(event, ['zone','arm']) is True
+
+    assert tag_match(event, ['partition,arm']) is True
+    assert tag_match(event, ['partition, arm']) is True
+    assert tag_match(event, ['partition, -arm']) is False
+
+
+def test_zone_generated_alarm_match(mocker):
+    label_provider = mocker.MagicMock(return_value="Beer")
+
+    payload = binascii.unhexlify('e2ff1cc414130b010f2c1801030000000000024f66666963652020202020202020202000d9')
+    raw = LiveEventMessage.parse(payload)
+    event_ = LiveEvent(raw, event_map, label_provider=label_provider)
+    assert "Zone Office in alarm" == event_.message
+
+    assert tag_match(event_, ['zone+alarm']) is True
+    label_provider.assert_called_once_with("zone", 3)
+
+    assert tag_match(event_, ['zone-alarm']) is False
+
+    # Live event label
+    assert tag_match(event_, ['zone,alarm,"Office"']) is True
+    assert tag_match(event_, ['zone,alarm,Office']) is True
+    assert tag_match(event_, ['zone,alarm,-Office']) is False
+    assert tag_match(event_, ['zone,alarm,Offic']) is False
+
+    # returned from label_provider
+    assert tag_match(event_, ['zone,alarm,Beer']) is True
+    assert tag_match(event_, ['zone,alarm,-Beer']) is False
