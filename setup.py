a6811f6a23c3d5fc565db08ac559f992df7fb1f5
João Paulo Barraca <jpbarraca@gmail.com>
2019-12-10 23:59:25 +0000

    Added to pypi

--- setup.py
+++ setup.py
@@ -6,20 +6,27 @@ from setuptools import setup, find_packages
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
-    name="paradox-alarm-interface",
-    version="1.2.0",
-    author="João Paulo Barraca",
-    author_email="jpbarraca@gmail.com",
-    description="Interface to Paradox Alarm Panels",
+    name='paradox-alarm-interface',
+    version='1.1.0',
+    author='João Paulo Barraca',
+    author_email='jpbarraca@gmail.com',
+    description='Interface to Paradox Alarm Panels',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jpbarraca/pai",
-    packages=find_packages(exclude=['tests', 'tests.*']),
-    install_requires=[],
+    long_description_content_type='text/markdown',
+    url='https://github.com/ParadoxAlarmInterface/pai',
+    download_url='https://github.com/ParadoxAlarmInterface/pai/archive/1.1.0.tar.gz',
+    packages=find_packages(exclude=['tests', 'tests.*', 'config.*', 'docs.*']),
+    scripts=[
+        'pai.py'
+    ],
+    install_requires=[
+        'construct >= 2.9.43',
+        'argparse >= 1.4.0'
+    ],
     setup_requires=[] + pytest_runner,
     tests_require=[
         'pytest',
@@ -29,17 +36,18 @@ setup(
         'mock'
     ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: EPL License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)',
+        'Operating System :: OS Independent',
     ],
     extras_require={
-        'YAML':  ["PyYAML>=3.13"]
+        'YAML':  ['PyYAML>=3.13']
     },
     entry_points={
         'console_scripts': [
             'ip150-connection-decrypt = paradox.console_scripts.ip150_connection_decrypt:main [YAML]',
             'pai-service = paradox.console_scripts.pai_run:main'
         ]
-    }
+    },
+    license='EPL'
 )
