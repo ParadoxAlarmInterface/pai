2947b6071fa98c67d508eb259d2ca4c456b97aec
Jevgeni Kiski <yozik04@gmail.com>
2020-04-24 16:45:02 +0300

    Partial `paradox-en` and `paradox-ru` encodings. Based on @ShadowofReason work.

--- tests/lib/test_encodings.py
+++ tests/lib/test_encodings.py
@@ -1,20 +1,62 @@
-from paradox.lib.encodings import register_encodings
+import pytest
+
+from paradox.lib.encodings import en, register_encodings, ru
 
 register_encodings()
 
-def test_en_encoding():
+test_data_en = [
+    (b"B", "B"),
+    (b"0", "0"),
+    (b"z", "z"),
+    (bytes([131]), "Ü"),
+    (bytes([142]), "ö"),
+    (bytes([148]), "ê"),
+    (bytes([151]), "ë"),
+    (bytes([153]), "Ä"),
+    (bytes([158]), "ä"),
+    (bytes([159]), "A"),
+    (bytes([161]), "Î"),
+    (bytes([162]), "Ì"),
+    (bytes([163]), "Í"),
+    (bytes([164]), "Ï"),
+    (bytes([177]), "±"),
+    (bytes([183]), "£"),
+    (bytes([185]), "⤈"),
+    (bytes([186]), "⤉"),
+    (bytes([206]), "Õ"),
+    (bytes([207]), "õ"),
+]
+
+
+@pytest.mark.parametrize("raw,expected", test_data_en)
+def test_en_encoding(raw, expected):
     encoding = "paradox-en"
-    assert b"B".decode(encoding) == "B"
+    assert len(expected) == 1
 
-    assert b"0".decode(encoding) == "0"
-
-    assert bytes([158]).decode(encoding) == "ä"
-    assert bytes([206]).decode(encoding) == "Õ"
-    assert bytes([131]).decode(encoding) == "Ü"
+    assert raw.decode(encoding) == expected, f"char {ord(raw)} != {expected}"
 
 
-def test_ru_encoding():
+def test_en_encoding_len():
+    assert len(en.decoding_table) == 224
+
+
+test_data_ru = [
+    (b"B", "B"),
+    (b"0", "0"),
+    (b"z", "z"),
+    (bytes([160]), "Б"),
+    (bytes([199]), "я"),
+    (bytes([230]), "щ"),
+]
+
+
+@pytest.mark.parametrize("raw,expected", test_data_ru)
+def test_ru_encoding(raw, expected):
     encoding = "paradox-ru"
-    assert b"B".decode(encoding) == "B"
+    assert len(expected) == 1
 
-    assert b"0".decode(encoding) == "0"
+    assert raw.decode(encoding) == expected, f"char {ord(raw)} != {expected}"
+
+
+def test_ru_encoding_len():
+    assert len(ru.decoding_table) == 256
