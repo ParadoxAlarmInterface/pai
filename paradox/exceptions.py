b045260875532b42dd334acc062e85993d4bbd4e
Jevgeni Kiski <yozik04@gmail.com>
2020-10-28 21:29:52 +0200

    Partition in code lockout detection to stop PAI from attempting to connect again.

--- paradox/exceptions.py
+++ paradox/exceptions.py
@@ -35,6 +35,8 @@ class PAICriticalException(PAIException):
 class AuthenticationFailed(PAICriticalException):
     pass
 
+class CodeLockout(PAICriticalException):
+    pass
 
 class PanelNotDetected(PAICriticalException):
     pass
