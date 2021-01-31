84c40454b6ee43e53fb454ff7830d1e21dcdd053
Jevgeni Kiski <yozik04@gmail.com>
2021-01-31 15:47:44 +0200

    Git tag and PAI version need to match during deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,6 +11,14 @@ if [ -z "$TRAVIS_TAG" ]; then
   DOCKER_IMAGE_TAG=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo "$TRAVIS_BRANCH-latest"; fi)
 else
   DOCKER_IMAGE_TAG="$TRAVIS_TAG"
+  if grep -q "$TRAVIS_TAG" paradox/__init__.py
+  then
+    echo "Git tag $TRAVIS_TAG matches VERSION in paradox/__init__.py"
+  else
+    echo "Git tag $TRAVIS_TAG does not match VERSION in paradox/__init__.py"
+    cat paradox/__init__.py
+    exit 1
+  fi
 fi
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
