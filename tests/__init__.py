5216f84952b696435b3b2b4e5138ad4d0bf99ce0
Jevgeni Kiski <yozik04@gmail.com>
2022-01-18 23:12:31 +0200

    Tests are missing custom encodings

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,20 +2,29 @@ import logging
 from os import path
 
 from paradox.config import config
+from paradox.lib.encodings import register_encodings
 
 config.load(path.join(path.dirname(__file__), "pai.conf"))
+
+
+def _configure_logger(name: str, handler: logging.Handler):
+    logger = logging.getLogger(name)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
 
 
 def configure_logger():
     FORMAT = (
         "%(asctime)s - %(levelname)-8s - %(threadName)-10s - %(name)s - %(message)s"
     )
-    logger = logging.getLogger("PAI")
+    logging.captureWarnings(True)
+
     logconsole_handler = logging.StreamHandler()
     logconsole_handler.setFormatter(logging.Formatter(FORMAT))
-    logger.addHandler(logconsole_handler)
 
-    logger.setLevel(logging.DEBUG)
+    _configure_logger('PAI', logconsole_handler)
+    _configure_logger('py', logconsole_handler)
 
 
 configure_logger()
+register_encodings()
