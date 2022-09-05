89988851b33f20e8f7956ada85a7189a452ad96e
Jevgeni Kiski <yozik04@gmail.com>
2022-09-05 14:50:47 +0300

    troubles do not have "live" tag

--- paradox/config.py
+++ paradox/config.py
@@ -182,7 +182,7 @@ class Config(object):
         "HOMEASSISTANT_NOTIFICATIONS_ALLOW_EVENTS": [],  # Same as before but as a white list. Default is use EVENT_FILTERS
         "HOMEASSISTANT_NOTIFICATIONS_EVENT_FILTERS": [  # list of tags, property changes to include or exclude. See event.py for tag list
             "live,alarm,-restore",
-            "live,trouble,-clock",
+            "trouble,-clock",
             "live,tamper",
         ],
         # Pushbullet
@@ -194,7 +194,7 @@ class Config(object):
         "PUSHBULLET_ALLOW_EVENTS": [],  # Same as before but as a white list. Default is use EVENT_FILTERS
         "PUSHBULLET_EVENT_FILTERS": [  # list of tags, property changes to include or exclude. See event.py for tag list
             "live,alarm,-restore",
-            "live,trouble,-clock",
+            "trouble,-clock",
             "live,tamper",
         ],
         "PUSHBULLET_MIN_EVENT_LEVEL": (
@@ -212,7 +212,7 @@ class Config(object):
         "PUSHOVER_ALLOW_EVENTS": [],  # Same as before but as a white list. Default is use EVENT_FILTERS
         "PUSHOVER_EVENT_FILTERS": [  # list of tags, property changes to include or exclude. See event.py for tag list
             "live,alarm,-restore",
-            "live,trouble,-clock",
+            "trouble,-clock",
             "live,tamper",
         ],
         "PUSHOVER_MIN_EVENT_LEVEL": (
@@ -227,7 +227,7 @@ class Config(object):
         "SIGNAL_ALLOW_EVENTS": [],  # Same as before but as a white list. Default is use EVENT_FILTERS
         "SIGNAL_EVENT_FILTERS": [  # list of tags, property changes to include or exclude. See event.py for tag list
             "live,alarm,-restore",
-            "live,trouble,-clock",
+            "trouble,-clock",
             "live,tamper",
         ],
         "SIGNAL_MIN_EVENT_LEVEL": (
