f3b99b0026a03b114b9a07055360d6411c528d1a
carcinogoy <github@noa.moe>
2022-12-01 13:16:02 +0200

    fixed bad commit

--- paradox/hardware/__init__.py
+++ paradox/hardware/__init__.py
@@ -33,6 +33,7 @@ def create_panel(core, start_communication_response: Container = None) -> Panel:
         "SPECTRA_SP6000",
         "SPECTRA_SP7000",
         "SPECTRA_SP65",
+        "SPECTRA_SP6000_PLUS",
         "MAGELLAN_MG5000",
         "MAGELLAN_MG5050",
         "MAGELLAN_MG5050_PLUS",
