1af730574192277dc71c970343e4747ade615d7b
Jevgeni Kiski <yozik04@gmail.com>
2026-08-07 23:10:05 +0300

    Removing obsolete files

--- paradox/connections/serial_connection.py
+++
@@ -1,6 +0,0 @@
-"""Deprecated location. Use :mod:`paradox.connections.serial.connection`."""
-
-from paradox.connections.serial.connection import SerialCommunication  # noqa: F401
-from paradox.connections.serial.protocol import (  # noqa: F401
-    SerialConnectionProtocol,
-)
--- paradox/connections/serial_encryption.py
+++
@@ -1,6 +0,0 @@
-"""Deprecated location. Use :mod:`paradox.connections.serial.encryption`."""
-
-from paradox.connections.serial.encryption import (  # noqa: F401
-    EncryptedSerialTransport,
-    make_serial_key,
-)
